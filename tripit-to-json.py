d5e1bf0e499350be4dbaa8556e155becd2c09083
Andrew Cole <andrew.cole@illallangi.com>
2021-01-30 21:55:21 +1100

    black && flake8

--- tripit-to-json.py
+++ tripit-to-json.py
@@ -1,15 +1,24 @@
-import os
 from json import dumps
+from os import environ
+
 from illallangi.tripitapi import API, JSONEncoder
 
-access_token=os.environ['TRIPIT_ACCESS_TOKEN']
-access_token_secret=os.environ['TRIPIT_ACCESS_TOKEN_SECRET']
-client_token=os.environ['TRIPIT_CLIENT_TOKEN']
-client_token_secret=os.environ['TRIPIT_CLIENT_TOKEN_SECRET']
+access_token = environ["TRIPIT_ACCESS_TOKEN"]
+access_token_secret = environ["TRIPIT_ACCESS_TOKEN_SECRET"]
+client_token = environ["TRIPIT_CLIENT_TOKEN"]
+client_token_secret = environ["TRIPIT_CLIENT_TOKEN_SECRET"]
 
-print(dumps(
-    API(access_token, access_token_secret, client_token, client_token_secret, cache=False),
-    cls=JSONEncoder,
-    indent=2,
-    sort_keys=True,
-))
+print(
+    dumps(
+        API(
+            access_token,
+            access_token_secret,
+            client_token,
+            client_token_secret,
+            cache=False,
+        ),
+        cls=JSONEncoder,
+        indent=2,
+        sort_keys=True,
+    )
+)
